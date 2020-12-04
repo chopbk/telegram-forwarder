9c7dc9ff5aac59c99f39689195769baf7b1c4469
tamnd12 <dinhtam94@gmail.com>
2020-12-04 19:06:15 +0700

    add function choose chat number and listen message

--- index.js
+++ index.js
@@ -1,67 +1,93 @@
-// TODO
-
-const api_id = "1519384";
-const api_hash = "c311ca044eac056e5b57e0cd4f94dbfc";
-// 1. Create an instance
-
+const dotenv = require("dotenv");
+dotenv.config();
 const { getSRPParams } = require("@mtproto/core");
-const readline = require("readline");
 const TelegramAPI = require("./telegram-api");
-const askForCode = () => {
-  return new Promise((resolve) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
 
-    rl.question("Please enter passcode for " + phone.num + ":\n", (num) => {
-      rl.close();
-      resolve(num);
-    });
+const api_id = process.env.api_id;
+const api_hash = process.env.api_hash;
+let inputInfo = {};
+let outputInfo = {};
+const {
+  getPhone,
+  getCode,
+  getPassword,
+  getNumberChoice,
+} = require("./helper/input");
+
+let telegramApi = new TelegramAPI({
+  api_id: api_id,
+  api_hash: api_hash,
+});
+const getInputOutputChannel = async () => {
+  console.log("Your chat list");
+  let chats = await telegramApi.getChats();
+  for (let i = 0; i < chats.length; i++) {
+    console.log(`${i} -- ${chats[i].id} -- ${chats[i].title} `);
+  }
+  console.log("Enter the number of Input Channel");
+  let input = await getNumberChoice();
+
+  inputInfo = {
+    id: chats[input].id,
+    title: chats[input].title,
+    access_hash: chats[input].access_hash,
+  };
+  console.log("Enter the number of Output Channel");
+  let output = await getNumberChoice();
+  outputInfo = {
+    id: chats[output].id,
+    title: chats[output].title,
+    access_hash: chats[output].access_hash,
+  };
+  return;
+};
+const startListen = async () => {
+  telegramApi.mtproto.updates.on("updates", (data) => {
+    let chats = data.chats;
+    if (chats[0].id !== inputInfo.id) return;
+    let updates = data.updates;
+    const newChannelMessages = updates
+      .filter((update) => update._ === "updateNewChannelMessage")
+      .map(({ message }) => message); // filter `updateNewChannelMessage` types only and extract the 'message' object
+
+    for (const message of newChannelMessages) {
+      // printing new channel messages
+      console.log(`[${message.to_id.channel_id}] ${message.message}`);
+    }
   });
 };
-const phone = "+84359637220";
-const password = "87654321";
+
 (async () => {
-  let telegramApi = new TelegramAPI({
-    api_id: api_id,
-    api_hash: api_hash,
-  });
   const user = await telegramApi.getUser();
   if (!user) {
-    const { phone_code_hash } = await telegramApi.sendCode(phone);
-    console.log(phone_code_hash);
-    let code = await askForCode();
+    // The user is not logged in
+    console.log("[+] You must log in");
+    const phone_number = await getPhone();
+    const { phone_code_hash } = await telegramApi.sendCode(phone_number);
+    const code = await getCode();
     try {
       const authResult = await telegramApi.signIn({
         code,
         phone,
         phone_code_hash,
       });
-
-      console.log(`authResult:`, authResult);
     } catch (error) {
       if (error.error_message !== "SESSION_PASSWORD_NEEDED") {
+        const { srp_id, current_algo, srp_B } = await telegramApi.getPassword();
+        const { salt1, salt2, g, p } = current_algo;
+        const { A, M1 } = await getSRPParams({
+          g,
+          p,
+          salt1,
+          salt2,
+          gB: srp_B,
+          password: await getPassword(),
+        });
+        await telegramApi.checkPassword({ srp_id, A, M1 });
         return;
       }
-
-      // 2FA
-
-      const { srp_id, current_algo, srp_B } = await telegramApi.getPassword();
-      const { g, p, salt1, salt2 } = current_algo;
-
-      const { A, M1 } = await getSRPParams({
-        g,
-        p,
-        salt1,
-        salt2,
-        gB: srp_B,
-        password,
-      });
-
-      const authResult = await telegramApi.checkPassword({ srp_id, A, M1 });
-
-      console.log(`authResult:`, authResult);
     }
   }
+  await getInputOutputChannel();
+  await startListen();
 })();
